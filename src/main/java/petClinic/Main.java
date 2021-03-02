d79dd57c9faf82373bd2b0f43fa9a47d9957482c
raluca-nedelcu <pop_raluca13@yahoo.com>
2021-03-02 19:47:04 +0200

    added gitignore

--- src/main/java/petClinic/Main.java
+++ src/main/java/petClinic/Main.java
@@ -1,7 +1,16 @@
 package petClinic;
 
+import java.util.Scanner;
+
+import static petClinic.service.PetClinicService.showMenu;
+import static petClinic.service.PetClinicService.showOption;
+
 public class Main {
     public static void main(String[] args) {
-
+        showMenu();
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        System.out.println(option);
+        showOption(option);
     }
 }
--- src/main/java/petClinic/service/PetClinicService.java
+++ src/main/java/petClinic/service/PetClinicService.java
@@ -1,5 +1,105 @@
 package petClinic.service;
 
-public class PetClinicService {
+import petClinic.model.Consult;
+import petClinic.model.Pet;
+import petClinic.model.Veterinarian;
+import petClinic.repository.ConsultDao;
+import petClinic.repository.PetDao;
+import petClinic.repository.VeterinarianDao;
 
+import java.util.Scanner;
+
+public class PetClinicService {
+    public static void showMenu() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - Print choice options.");
+        System.out.println("\t 1 - Create");
+        System.out.println("\t 2 - Update");
+        System.out.println("\t 3 - Delete");
+        System.out.println("\t 4 - List/Find");
+    }
+
+    public static void showOption(int option) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+
+        showOption(option);
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 0:
+                    showMenu();
+                    break;
+                case 1:
+                    create();
+                    break;
+                case 2:
+                    update();
+                    break;
+                case 3:
+                    delete();
+                    break;
+                case 4:
+                    find();
+                    break;
+                case 5:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void showCreateMenu() {
+        System.out.println("\t 0 - Veterinarian");
+        System.out.println("\t 1 - Pet");
+        System.out.println("\t 2 - Consult");
+
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        System.out.println("Your choise is: " + option);
+    }
+
+    public static void create() {
+        Scanner scanner = new Scanner(System.in);
+        VeterinarianDao veterinarianDao = new VeterinarianDao();
+        Veterinarian veterinarian = new Veterinarian();
+        veterinarianDao.createVeterinarian(veterinarian);
+    }
+
+
+    public static void update() {
+        Scanner scanner = new Scanner(System.in);
+        VeterinarianDao veterinarianDao = new VeterinarianDao();
+        Veterinarian veterinarian1 = veterinarianDao.findByIdVeterinarian(1L);
+        veterinarian1.setFirstName(scanner.nextLine());
+        veterinarianDao.updateVeterinarian(veterinarian1);
+
+        ConsultDao consultDao = new ConsultDao();
+        Consult consult = consultDao.findByIdConsult(1L);
+        consult.setDescription(scanner.nextLine());
+        consultDao.updateConsult(consult);
+
+        PetDao petDao = new PetDao();
+        Pet pet = petDao.findByIdPet(1L);
+        pet.setRace(scanner.nextLine());
+        petDao.updatePet(pet);
+
+    }
+
+    public static void delete() {
+        Scanner scanner = new Scanner(System.in);
+        VeterinarianDao veterinarianDao = new VeterinarianDao();
+    }
+
+    public static void find() {
+        Scanner scanner = new Scanner(System.in);
+        VeterinarianDao veterinarianDao = new VeterinarianDao();
+        Veterinarian veterinarian = veterinarianDao.findByIdVeterinarian(1L);
+        String veterinarian1 = scanner.nextLine();
+        System.out.println(veterinarian1);
+    }
 }
